0fe879ba20bff9b5b44c6b9fb4fe2357a2f20ca8
Johannes Kroop <johkroop@gmail.com>
2018-06-14 21:02:11 +0200

    fix athenticity token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, except: %i[create update destroy]
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
